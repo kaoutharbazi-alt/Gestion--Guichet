4e448dd057801cf9f159fb9596035ef7203a3d01
kaouthar <kawtarbaze31@gmail.com>
2026-03-26 21:33:31 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import AdminPage from "./components/AdminPage";
+import DashboardAdmin from "./pages/DashboardAdmin";
+import Demandes from "./pages/Demandes";
+import Admin from "./pages/Admin";
+import Etudiant from "./pages/Etudiant";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/etudiant" element={<Etudiant />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/dashboard" element={<Admin />} />
+        <Route path="/demandes" element={<Demandes />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
